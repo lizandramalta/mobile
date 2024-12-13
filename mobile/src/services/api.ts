e168d639e560cfdd74675f8f07bcb316b5b2fdf5
Lizandra Malta <lizlrmalta@gmail.com>
2024-12-13 00:26:31 -0300

    criando uma variável de ambiente para armazenar o ip

--- mobile/src/services/api.ts
+++ mobile/src/services/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
 export const api = axios.create({
-  baseURL: 'http://192.168.2.160:3333',
+  baseURL: `http://${process.env.EXPO_PUBLIC_IP_ADDRESS}:3333`,
   timeout: 700
 })
